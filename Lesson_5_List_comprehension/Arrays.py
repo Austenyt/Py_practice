98c395b803b7d4a6f80c96825628634031de4aaa
Austenyt <austenyt@gmail.com>
2025-03-22 00:10:31 +0300

    Arrays

---
+++ Lesson_5_List_comprehension/Arrays.py
@@ -0,0 +1,82 @@
+"""
+Диагональ матрицы: Создайте список, состоящий из диагонали квадратной матрицы (список списков).
+"""
+# [1, 4, 7],
+# [2, 5, 8],
+# [3, 6, 9]
+# matrix = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# l_str = [matrix[i][i] for i in range(len(matrix))] # Прямая диагональ
+# l = [matrix[i][len(matrix[i]) - 1 - i] for i in range(len(matrix))] # Обратная диагональ
+# print(l_str)
+# print(l)
+"""
+Даны два списка, создать третий, сложив элементы по индексам.
+"""
+# lst_1 = [1,2,3]
+# lst_2 = [4,5,6]
+# lst_3 = [lst_1[i] + lst_2[i] for i in range(len(lst_1))]
+# print(lst_3)
+"""
+Даны два списка, сложить их элементы по индексам, но брать элементы из второго списка в обратном порядке.
+lst_1 = [1, 2, 3, 4]
+lst_2 = [5, 6, 7, 8]
+
+результат = [9, 9, 9, 9]
+"""
+# lst_1 = [1, 2, 3, 4]
+# lst_2 = [5, 6, 7, 8]
+# lst_3 = [lst_1[i] + lst_2[-i-1] for i in range(len(lst_1))]
+# print(lst_3)
+"""
+Даны две квадратные матрицы одинаковой размерности, нужно их сложить в новую матрицу.
+"""
+# array_1 = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# array_2 = [
+#     [1, 4, 7],
+#     [2, 5, 8],
+#     [3, 6, 9]
+# ]
+# lst_1 = [array_1[i-i][i] + array_2[i-i][i] for i in range(len(array_1))]
+# lst_2 = [array_1[i-i+1][i] + array_2[i-i+1][i] for i in range(len(array_1))]
+# lst_3 = [array_1[i-i+2][i] + array_2[i-i+2][i] for i in range(len(array_1))]
+# print(lst_1)
+# print(lst_2)
+# print(lst_3)
+"""
+Дана матрица, вывести обе её диагонали задом наперед (2 for или 2 генератора)
+допустим матрица от 1 до 9
+результат:
+9 5 1
+7 5 3
+"""
+# array = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# lst_1 = [array[2-i][2-i] for i in range(len(array))]
+# lst_2 = [array[i][2-i] for i in range(len(array))]
+# print(lst_1)
+# print(lst_2)
+"""
+Транспонировать матрицу.
+"""
+array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+lst_1 = [array[i][i-i] for i in range(len(array))]
+lst_2 = [array[i][i-i+1] for i in range(len(array))]
+lst_3 = [array[i][i-i+2] for i in range(len(array))]
+print(lst_1)
+print(lst_2)
+print(lst_3)
--- Lesson_5_List_comprehension/Theory.py
+++ Lesson_5_List_comprehension/Theory.py
@@ -1,49 +1,67 @@
 """
 Создайте список чисел от 1 до 20
 """
-lst = []
-for i in range(1,21):
-    lst.append(i)
-print(lst)
+# lst = []
+# for i in range(1,21):
+#     lst.append(i)
+# print(lst)
 
-result = [c for c in range(1,21)]
-print(result)
-
-a = list(range(1,21))
-print(a)
+# result = [c for c in range(1,21)]
+# print(result)
+#
+# a = list(range(1,21))
+# print(a)
 """
 Создайте список четных чисел от 20 до 0
 """
-a = list(range(20,-1,-2))
-print(a)
+# a = list(range(20,-1,-2))
+# print(a)
 """
 Создайте список из слов длиной более 3
 """
-words = ["cat", "elephant", "dog", "bird", "lion"]
-lst_2 = [el for el in words if len(el) > 3]
-print(lst_2)
+# words = ["cat", "elephant", "dog", "bird", "lion"]
+# lst_2 = [el for el in words if len(el) > 3]
+# print(lst_2)
 """
 Создайте генератор списка, который генерирует квадраты нечетных чисел от 1 до 15.
 """
-result = [c**2 for c in range(1,16,2)]
-print(result)
+# result = [c**2 for c in range(1,16,2)]
+# print(result)
 """
 Создайте список отрицательных чисел
 """
-numbers = [-5, 3, -2, 9, 0, -1]
-numbers_2 =[num for num in numbers if num < 0]
-print(numbers_2)
+# numbers = [-5, 3, -2, 9, 0, -1]
+# numbers_2 =[num for num in numbers if num < 0]
+# print(numbers_2)
 """
 Создайте список, заменив число на "четный" или "нечетный"
 """
-numbers = [-5, 3, -2, 9, 0, -1]
-lst = ["четный" if num % 2 == 0 else "нечетный" for num in numbers]
-print(lst)
+# numbers = [-5, 3, -2, 9, 0, -1]
+# lst = ["четный" if num % 2 == 0 else "нечетный" for num in numbers]
+# print(lst)
 
 """
 Создайте генератор списка, который для чисел из списка увеличивает все значения больше 100 на 10%, а все 
 остальные оставляет без изменений.
 """
-numbers = [50, 120, 200, 80, 150]
-numbers_2 = [num * 1.1 if num > 100 else num for num in numbers]
-print(numbers_2)
+# numbers = [50, 120, 200, 80, 150]
+# numbers_2 = [num * 1.1 if num > 100 else num for num in numbers]
+# print(numbers_2)
+
+# 1. Кол-ва итераций
+for _ in range(10):
+    pass
+
+# 2. Перебор значений
+for el in "hello":
+    pass
+
+# 3. Перебор посл. чисел.
+for num in range(1, 101, 2):
+    pass
+
+# 4. Перебор индексов
+#      0  1   2      3     4
+lst = [5, 8, -10, 'hello', 9]
+for i in range(len(lst) - 1):
+    print(lst[i],lst[i + 1])
