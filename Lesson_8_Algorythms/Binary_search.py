2d27e5973f9b2bbaa859debf44f5d6b056e5d275
austenyt <austenyt@gmail.com>
2025-04-10 01:07:22 +0300

    ghgjhj

---
+++ Lesson_8_Algorythms/Binary_search.py
@@ -0,0 +1,11 @@
+def binary_search(lst, a):
+    lst1 = lst[:int(len(lst) / 2)]
+    lst2 = lst[int(len(lst) / 2):]
+    if a in lst1:
+        min_a =
+    return lst1
+
+
+lst = [5, 2, 8, 7, 1, 6, 4, 3]
+a = 6
+print(binary_search(lst, a))
