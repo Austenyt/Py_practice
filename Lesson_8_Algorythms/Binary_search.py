57de7a89543137e38fc94e6e6f60fca793f2fa19
austenyt <austenyt@gmail.com>
2025-04-10 23:50:49 +0300

    ghgjhj

--- Lesson_8_Algorythms/Binary_search.py
+++ Lesson_8_Algorythms/Binary_search.py
@@ -1,28 +1,17 @@
-# def binary_search(lst, a):
-#     lst1 = lst[:int(len(lst) / 2)]
-#     lst2 = lst[int(len(lst) / 2):]
-#     if a in lst1:
-#         min_a = min(lst1)
-#         max_a = max(lst1)
-#         av = (max_a - min_a) / 2
-#     else:
-#         min_a = min(lst2)
-#         max_a = max(lst2)
-#         av = (max_a - min_a) / 2
-#     return lst1
-#
-#
-# lst = [5, 2, 8, 7, 1, 6, 4, 3]
-# a = 6
-# print(binary_search(lst, a))
+def binary_search(lst, a):
+
+    first = 0
+    last = len(lst) - 1
+    while last >= first:
+        av = (first + last) // 2
+        if lst[av] == a:
+            return av
+        elif a < lst[av]:
+            last = av - 1
+        else:
+            first = av + 1
+
+
 lst = [1, 3, 4, 6, 7, 8, 10, 13, 14, 18, 19, 21, 24, 37, 40, 45, 71]
-a = 10
-b = len(lst)
-min_a = 0
-max_a = 0
-while min_a < a < max_a:
-    first = lst[0]
-    last = lst[int(len(lst)) - 1]
-    av = lst[len(lst) // 2]
-    lst = lst[:av]
-    if a in lst:dfdf
+a = 13
+print(binary_search(lst, a))
